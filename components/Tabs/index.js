8af9bcf0f3e9c96cd552860021b265dab39de679
Nick <hansen.nick95@gmail.com>
2019-07-22 14:42:25 -0700

    Finished MVP with readme questions

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,29 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+console.log(axios.get('https://lambda-times-backend.herokuapp.com/topics'))
+
+class Tab {
+    constructor(topic) {
+        let tabDiv = document.createElement('div');
+        tabDiv.classList.add('tab');
+        tabDiv.textContent = topic
+
+        return tabDiv;
+    }
+}
+
+let topicsElement = document.querySelector('.topics');
+console.log(topicsElement)
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then((response) => {
+        response.data.topics.forEach((topic) => {
+            topicsElement.appendChild(new Tab(topic))
+        }) 
+    })
+    
+    .catch((err) => {
+        console.log('error: ', err)
+    })
